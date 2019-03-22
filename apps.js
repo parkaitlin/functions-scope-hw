f964c4a84afa85f0ddf8da9ce73870753fb4a626
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-03-21 17:11:21 -0700

    Commit 1 - Verbal Questions

---
+++ apps.js
@@ -0,0 +1,13 @@
+console.log('Homework!');
+
+//1. a parameter are input values we can include when we 
+    //declare a function. An argument is when we actually invoke 
+    //a function.
+//2. Return 'exits' or termnates the the block of code and sends 
+    //the output to the console when the function is invoked. 
+    //Console.log does not affect our code like return, it simply 
+    //outputs whatever is being invoked.
+//3. For a function, to return a value "return ()" needs to be 
+    // included to stop the code and send an output to the console.
+
+    
